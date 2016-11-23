775e2adb2eea35e357fe60a1427874a47b92648c
Your Name <you@example.com>
2016-11-23 10:49:14 +0100

    add exercise 6

---
+++ maveProject/kbe_ue6_1-annotation/src/main/java/de/htw_berlin/ai_bachelor/kbe/MyAnnotationTester.java
@@ -0,0 +1,10 @@
+package de.htw_berlin.ai_bachelor.kbe;
+
+@CreationInfo(description="Diese Klasse hat einen Sinn", tags={"Unsinnlos", "Test"})
+public class MyAnnotationTester  {
+	
+	public void foo() {
+		
+	}
+
+}
